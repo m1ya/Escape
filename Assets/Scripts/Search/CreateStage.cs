bcbef96a7b8e35e254a102ea095b587f4320acc4
m1ya <o_izy@yahoo.co.jp>
2017-11-02 01:00:33 +0900

    荒削りな移動・拡大

---
+++ Assets/Scripts/Search/CreateStage.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CreateStage : MonoBehaviour
+{
+
+	//name, parent, mode, fileName, width, height, x, y, z
+	private CSVData pointDatas;
+	private GameObject searchCanvas;
+
+	public CreateStage (GameObject canvas)
+	{
+		searchCanvas = canvas;
+
+		SetPointData ("RegisterPoints");
+		CreateParent ();
+		CreateActionObj ();
+	}
+
+	//ポイントファイルをセットする
+	public void SetPointData (string fileName)
+	{
+		pointDatas = CSVReader.ReadCsv ("Points/" + fileName);
+	}
+
+	//Zoom | Move となる親オブジェクトを生成する
+	void CreateParent ()
+	{
+		for (int i = 0; i < pointDatas.lineNum; i++) {
+			if (pointDatas.datas [i] [2] == "Move" || pointDatas.datas [i] [2] == "Zoom") {
+				if (pointDatas.datas [i] [2] == "Move") {
+					PointAction.Instance.AddRoom (int.Parse (pointDatas.datas [i] [0]));
+				}
+				GameObject obj = new GameObject (pointDatas.datas [i] [0]);
+				obj.transform.SetParent (searchCanvas.transform);
+				obj.transform.localPosition = Vector3.zero;
+				PointAction.Instance.AddParents (pointDatas.datas [i] [0], obj);
+				obj.SetActive (false);
+				Texture texture = Resources.Load ("Images/Backgrounds/" + pointDatas.datas [i] [3]) as Texture;
+				if (texture == null) {
+					Debug.LogError ("そのようなファイルは存在しません：" + pointDatas.datas [i] [3]);
+				}
+				PointAction.Instance.AddBg (pointDatas.datas [i] [0], texture);
+			}
+		}
+	}
+
+	// Check などのアクションがあるオブジェクトを生成する
+	void CreateActionObj ()
+	{
+		for (int i = 0; i < pointDatas.lineNum; i++) {
+			if (pointDatas.datas [i] [2] == "Check" || pointDatas.datas [i] [2] == "Zoom") {
+				string name = pointDatas.datas [i] [0];
+				GameObject obj = new GameObject (name);
+				RawImage img = obj.AddComponent<RawImage> ();
+				Texture texture = Resources.Load ("Images/ActionObjs/" + pointDatas.datas [i] [3]) as Texture;
+				if (texture == null) {
+					Debug.LogError ("そのようなファイルは存在しません：" + pointDatas.datas [i] [3]);
+				}
+				img.texture = texture;
+				obj.transform.SetParent (PointAction.Instance.GetParents (pointDatas.datas [i] [1]).transform);
+				RectTransform rectTransform = obj.GetComponent<RectTransform> ();
+				if (pointDatas.datas [i] [4] != "" && pointDatas.datas [i] [5] != "")
+					rectTransform.sizeDelta = new Vector2 (int.Parse (pointDatas.datas [i] [4]), int.Parse (pointDatas.datas [i] [5]));
+				if (pointDatas.datas [i] [6] != "" && pointDatas.datas [i] [7] != "" && pointDatas.datas [i] [8] != "")
+					rectTransform.localPosition = new Vector3 (int.Parse (pointDatas.datas [i] [6]), int.Parse (pointDatas.datas [i] [7]), int.Parse (pointDatas.datas [i] [8]));
+				PointAction.Instance.AddActionObj (name, obj);
+
+				//TODO: Zoom後とZoomする前のトリガーの画像が同じになる
+				if (pointDatas.datas [i] [2] == "Zoom") {
+					EventTrigger trigger = obj.AddComponent<EventTrigger> ();
+					var entry = new EventTrigger.Entry ();
+					entry.eventID = EventTriggerType.PointerDown;
+					entry.callback.AddListener ((x) => {
+						PointAction.Instance.ZoomRoom (name);
+					});
+					trigger.triggers.Add (entry);
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/Search/PointAction.cs
+++ Assets/Scripts/Search/PointAction.cs
@@ -1,22 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PointAction : MonoBehaviour
+public class PointAction : SingletonMonoBehaviour<PointAction>
 {
 
-	private CSVData pointData;
+	private GameObject searchCanvas;
+	[SerializeField]
+	private RawImage backGround;
 
-	//ポイントファイルをセットする
-	void SetPointData (string fileName)
+	private Dictionary<string, GameObject> parents = new Dictionary<string, GameObject> ();
+	private Dictionary<string, GameObject> actionObj = new Dictionary<string, GameObject> ();
+	private Dictionary<string, Texture> bg = new Dictionary<string, Texture> ();
+
+	private List<int> mainRoom = new List<int> ();
+	private int currentRoom;
+	private string zoomPoint;
+
+	public void AddParents (string name, GameObject obj)
 	{
-		pointData = CSVReader.ReadCsv ("Points/" + fileName);
+		parents.Add (name, obj);
 	}
+
+	public void AddRoom (int roomNum)
+	{
+		mainRoom.Add (roomNum);
+	}
+
+	public GameObject GetParents (string name)
+	{
+		return parents [name];
+	}
+
+	public void AddBg (string name, Texture texture)
+	{
+		bg.Add (name, texture);
+	}
+
+	public void AddActionObj (string name, GameObject obj)
+	{
+		actionObj.Add (name, obj);
+	}
+
+	private void SetBgImage (string name)
+	{
+		backGround.texture = bg [name];
+	}
+
+	public void MoveLeft ()
+	{
+		int oldRoom = currentRoom;
+		currentRoom--;
+		if (currentRoom < 0) {
+			currentRoom = mainRoom.Count - 1;
+		}
+		ChangeRoom (oldRoom, currentRoom);
+	}
+
+	public void MoveRight ()
+	{
+		int oldRoom = currentRoom;
+		currentRoom++;
+		if (currentRoom >= mainRoom.Count) {
+			currentRoom = 0;
+		}
+		ChangeRoom (oldRoom, currentRoom);
+	}
+
+	public void ReturnRoom ()
+	{
+		ChangeRoom (-1, currentRoom);
+	}
+
+	private void ChangeRoom (int from, int to)
+	{
+		//TODO: Roomの時は左右をだす、zoomの時は後ろを出す
+		if (from != -1)
+			parents [mainRoom [from].ToString ()].SetActive (false);
+		if (zoomPoint != null) {
+			parents [zoomPoint].SetActive (false);
+			zoomPoint = null;
+		}
+
+		SetBgImage (mainRoom [to].ToString ());
+		parents [mainRoom [to].ToString ()].SetActive (true);
+	}
+
+	public void ZoomRoom (string to)
+	{
+		zoomPoint = to;
+		parents [mainRoom [currentRoom].ToString ()].SetActive (false);
+		SetBgImage (to);
+		parents [to].SetActive (true);
+	}
+
 
 	// Use this for initialization
 	void Start ()
 	{
-		SetPointData ("RegisterPoints");
+		searchCanvas = this.gameObject;
+
+		new CreateStage (searchCanvas);
+		mainRoom.Sort ();
+
+		//一番数字の若いステージのみを表示する
+		currentRoom = 0;
+		ChangeRoom (-1, currentRoom);
+
+
 	}
 	
 	// Update is called once per frame
@@ -24,6 +116,4 @@ public class PointAction : MonoBehaviour
 	{
 		
 	}
-
-	//TODO: Moveを生成 Zoomを生成 その他を生成（Move,Zoomのこ）
 }
